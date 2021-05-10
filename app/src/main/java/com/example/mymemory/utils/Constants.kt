9d64a017dda95923c4317c28adc025b04da8706f
Simon Sahlin <simon.sahlin@utb.ecutbildning.se>
2021-05-10 16:54:06 +0200

    fist commit

---
+++ app/src/main/java/com/example/mymemory/utils/Constants.kt
@@ -0,0 +1,18 @@
+package com.example.mymemory.utils
+
+import com.example.mymemory.R
+
+val DEFAULT_ICONS = listOf(
+        R.drawable.ic_alarm,
+        R.drawable.ic_apartment,
+        R.drawable.ic_attach,
+        R.drawable.ic_bathtub,
+        R.drawable.ic_cake,
+        R.drawable.ic_camera,
+        R.drawable.ic_face,
+        R.drawable.ic_flat,
+        R.drawable.ic_moon,
+        R.drawable.ic_sun,
+        R.drawable.ic_tractor,
+        R.drawable.ic_umbrella
+)
